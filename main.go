af996f184ed3595eb0dd95fe4710365f39149072
samaslima <samaslimag@gmail.com>
2025-04-08 19:31:18 -0300

    add method post

--- main.go
+++ main.go
@@ -6,20 +6,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getPizzas(c *gin.Context) {
-	var pizzas = []models.Pizza{
-		{ID: 1, Name: "calabresa", Price: 40.5},
-		{ID: 2, Name: "frango", Price: 35.4},
-		{ID: 3, Name: "marguerita", Price: 30.4},
-	}
+var pizzas = []models.Pizza{
+	{ID: 1, Name: "calabresa", Price: 40.5},
+	{ID: 2, Name: "frango", Price: 35.4},
+	{ID: 3, Name: "marguerita", Price: 30.4},
+}
 
+func getPizzas(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"pizzas": pizzas,
 	})
 }
 
+func addPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	pizzas = append(pizzas, newPizza)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/pizzas", getPizzas)
+	router.POST("/pizzas", addPizzas)
 	router.Run()
 }
